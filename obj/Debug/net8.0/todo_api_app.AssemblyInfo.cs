84d8e522a57d269598afa3354a04f038158080c6
Marcus Neo <marcus121neo@gmail.com>
2024-05-21 18:31:34 +0800

    Merge Authentication to Main

--- obj/Debug/net8.0/todo_api_app.AssemblyInfo.cs
+++ obj/Debug/net8.0/todo_api_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo_api_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc25a8520a01e2c9cc48e413221a84e9a1946d24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20859db78a625b686c0695242e27a7b3d3634688")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo_api_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo_api_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
